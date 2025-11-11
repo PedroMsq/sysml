640422876f11c315c79cc6ec8f3767216050569c
V1Barros <vbarros562@gmail.com>
2025-11-11 19:00:14 -0300

    flowadapter navigation

--- src/main/java/adapters/nodes/FlowAdapter.java
+++ src/main/java/adapters/nodes/FlowAdapter.java
@@ -7,6 +7,7 @@ import org.eclipse.emf.common.util.EList;
 import org.omg.sysml.lang.sysml.ActionUsage;
 import org.omg.sysml.lang.sysml.Classifier;
 import org.omg.sysml.lang.sysml.Feature;
+import org.omg.sysml.lang.sysml.FeatureChainExpression;
 import org.omg.sysml.lang.sysml.FeatureReferenceExpression;
 import org.omg.sysml.lang.sysml.FlowEnd;
 import org.omg.sysml.lang.sysml.FlowUsage;
@@ -17,6 +18,7 @@ import org.omg.sysml.lang.sysml.Redefinition;
 import org.omg.sysml.lang.sysml.ReferenceSubsetting;
 import org.omg.sysml.lang.sysml.Usage;
 
+import adapters.utils.NamedElementAdapter;
 import interfaces.nodes.IFlow;
 import interfaces.structure.IItemUsage;
 import interfaces.utils.INamedElement;
@@ -26,70 +28,73 @@ import org.omg.sysml.lang.sysml.Expression;
 import org.omg.sysml.lang.sysml.Namespace;
 import org.omg.sysml.lang.sysml.PayloadFeature;
 
-public class FlowAdapter implements IFlow {
+public class FlowAdapter extends NamedElementAdapter implements IFlow {
 
-    private final Element flowElement;   // referência direta ao FlowUsage
-    private IItemUsage source;     // origem
-    private IItemUsage target;     // destino
-    private INamedElement payload; // payload (se definido) 
+	private final Element flowElement; // referência direta ao FlowUsage
+	private IItemUsage source; // origem
+	private IItemUsage target; // destino
+	private INamedElement payload; // payload (se definido)
 
-    public FlowAdapter(Element flowElement, Namespace containerNamespace) {
-        this.flowElement = flowElement;
+	public FlowAdapter(Element flowElement, Namespace containerNamespace) {
+		super(flowElement);
+		this.flowElement = flowElement;
 
-        for (Element elem : containerNamespace.getOwnedMember()) {
-        	if (elem instanceof FlowUsage fu) {
-        		
-        		for (Feature fa : fu.getRelatedFeature()) {
-        			System.out.println("fu_name: " + fu.getName());
-        		}
-        		
-        		for (Feature fb : fu.getOwnedFeature()) {
-        			if (fb instanceof PayloadFeature pf) {
-        				for (Feature fc : pf.getFeature()) {
-        					System.out.println("payload_feature: " + fc.getName());
-        				}
-        			}
-        			if (fb instanceof FlowEnd fe) {
-        				
-        				for(Feature features : fe.getOwnedFeature()) {
-        					if (features instanceof Usage u) {
-        						System.out.println("usage_owner: " + u.getOwner().getName());
-        						System.out.println("usage: " + u.getName());
-        					}
-        					System.out.println("fe_feature: " + features.getName());
-        				}
-        			}
-        			
-        		}
-        	}
-        }
-    }
+		for (Element elem : containerNamespace.getOwnedMember()) {
+			if (elem instanceof FlowUsage fu) {
 
+				for (Feature fa : fu.getRelatedFeature()) {
+					System.out.println("fu_name: " + fu.getName()); // flow1 ou flow2
+				}
 
+				for (Feature fb : fu.getOwnedFeature()) {
+					if (fb instanceof PayloadFeature pf) {
+						System.out.println("payload_feature: " + fb.getType().get(0).getName()); // Fuel
+					}
 
-    @Override
-    public IItemUsage getSource() {
-        return source;
-    }
+					if (fb instanceof FlowEnd fe) {
+						ReferenceSubsetting refsub = fe.getOwnedReferenceSubsetting();
+						for (Feature fif : refsub.getReferencedFeature().getChainingFeature()) {
+							System.out.println(fif.getName());
+						}
+						System.out.println(refsub.getReferencingFeature().getName()); // source ou target
+						System.out.println(refsub.getOwningFeature().getName()); // source ou target
+						for (Feature features : fe.getOwnedFeature()) {
+							if (features instanceof Usage u) {
+								System.out.println("usage_owner: " + u.getOwner().getName()); // source ou target
+								System.out.println("usage: " + u.getName()); // fuelSupply ou fuelReturn
+							}
+							// System.out.println("fe_feature: " + features.getName()); fuelSupply ou fuelReturn
+						}
+					}
 
-    @Override
-    public IItemUsage getTarget() {
-        return target;
-    }
+				}
+			}
+		}
+	}
 
-    @Override
-    public INamedElement getPayload() {
-        return payload;
-    }
+	@Override
+	public IItemUsage getSource() {
+		return source;
+	}
 
-    @Override
-    public String getName() {
-        return flowElement.getName();
-    }
+	@Override
+	public IItemUsage getTarget() {
+		return target;
+	}
 
-    @Override
-    public String getDeclaredName() {
-        return flowElement.getDeclaredName();
-    }
-    
+	@Override
+	public INamedElement getPayload() {
+		return payload;
+	}
+
+	@Override
+	public String getName() {
+		return flowElement.getName();
+	}
+
+	@Override
+	public String getDeclaredName() {
+		return flowElement.getDeclaredName();
+	}
+
 }
